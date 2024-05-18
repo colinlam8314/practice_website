512049aa3c56f4803819b948e7e43514e876dc9b
colinlam8314 <thefantasticcolin@gmail.com>
2024-05-17 18:26:23 -0700

    data structure

--- pages/04_Strain_Database.py
+++ pages/04_Strain_Database.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 
-df = pd.read_json('config.json')
 
-edited_df = st.data_editor(df)
+# for sample_type_df in sample_types:
+#     edited_df = st.data_editor(sample_type_df)
+df = {
+    "rna target": [
+        {
+            "index": "covid",
+            "strain": "ab",
+            "lot": "!234342",
+            "date received": datetime.today() 
+        }
+    ]
+}
+
+loaded_df = pd.DataFrame(df['rna target'])
+
+# sample_types = []
+# for sample_type in loaded_df.columns:
+#     sample_types.append(loaded_df[sample_type])
+
+# st.write(sample_types)
+st.data_editor(loaded_df,hide_index=True)
