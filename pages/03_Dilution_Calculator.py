738458732ebf8d6c51f6d126a8a8479b188124f9
colinlam8314 <thefantasticcolin@gmail.com>
2024-07-19 11:29:48 -0700

    dna bacteria strain update

--- pages/03_Dilution_Calculator.py
+++ pages/03_Dilution_Calculator.py
@@ -11,34 +11,53 @@ st.markdown("<h1 style='text-align: center;'>Dilution Calculator</h1>", unsafe_a
 
 
 # Dilution calculation function
-def dilute_calc(conc, wanted_conc):
-    if wanted_conc <= 0:
-        st.markdown("Please input a valid number")
+# def dilute_calc(conc, wanted_conc):
+#     if wanted_conc <= 0:
+#         st.markdown("Please input a valid number")
 
+#     volume = (2*conc)/wanted_conc
+
+#     if volume >= 50 and volume <= 1000:
+#         volume = volume - 2
+#         # st.write('2 uL ',conc,' + ',volume,'uL H2O --> ',volume+2,' E4 cp/uL')
+#         st.markdown(f'2 uL <mark><b>{conc:.2e}</b></mark> cp/uL + <mark><b>{volume}</b></mark> uL H2O --> <mark><b>{volume+2}</b></mark> <mark><b>{wanted_conc:.2e}</b></mark> cp/uL',unsafe_allow_html=True)
+
+#     if volume > 1000:
+#         conc_dilute_100x = conc/100
+#         st.markdown(f'2 uL <mark><b>{conc:.2e}</b></mark> cp/uL stock + 198 uL H2O --> 200 uL <mark><b>{conc_dilute_100x:.2e}</b></mark> cp/uL', unsafe_allow_html=True)
+#         dilute_calc(conc_dilute_100x, wanted_conc)
+
+#     if volume < 50 and volume > 2 and conc != float(final_conc):
+#         conc_dilute_10x = conc/10
+#         st.markdown(f'20 uL <mark><b>{conc:.2e}</b></mark> cp/uL stock + 180 uL H2O --> 200 uL <mark><b>{conc_dilute_10x:.2e}</b></mark> cp/uL', unsafe_allow_html=True)
+#         dilute_calc(conc_dilute_10x, wanted_conc) 
+#     elif conc == float(final_conc) and volume < 50 and volume > 2:
+#         volume = volume - 2
+#         # st.write('2 uL ',conc,' + ',volume,'uL H2O --> ',volume+2,' E4 cp/uL')
+#         st.markdown(f'2 uL <mark><b>{conc:.2e}</b></mark> cp/uL + <mark><b>{volume}</b></mark> uL H2O --> <mark><b>{volume+2}</b></mark> <mark><b>{wanted_conc:.2e}</b></mark> cp/uL',unsafe_allow_html=True)
+#         st.markdown("Starting concentration might be too diluted. Consider changing the desired concentration or use a more concentrated stock.")
+#     else:
+#         st.markdown("Input an appropriate target concentration!")
+
+dilution_num = 0
+
+def dilute_calc(conc, wanted_conc):
     volume = (2*conc)/wanted_conc
+    count = dilution_num
 
     if volume >= 50 and volume <= 1000:
         volume = volume - 2
         # st.write('2 uL ',conc,' + ',volume,'uL H2O --> ',volume+2,' E4 cp/uL')
         st.markdown(f'2 uL <mark><b>{conc:.2e}</b></mark> cp/uL + <mark><b>{volume}</b></mark> uL H2O --> <mark><b>{volume+2}</b></mark> <mark><b>{wanted_conc:.2e}</b></mark> cp/uL',unsafe_allow_html=True)
 
-    if volume > 1000:
-        conc_dilute_100x = conc/100
-        st.markdown(f'2 uL <mark><b>{conc:.2e}</b></mark> cp/uL stock + 198 uL H2O --> 200 uL <mark><b>{conc_dilute_100x:.2e}</b></mark> cp/uL', unsafe_allow_html=True)
-        dilute_calc(conc_dilute_100x, wanted_conc)
+    if volume < 50 and count == 0:
+        st.write("Starting concentration is too low!")
 
-    if volume < 50 and volume > 2 and conc != float(final_conc):
-        conc_dilute_10x = conc/10
-        st.markdown(f'20 uL <mark><b>{conc:.2e}</b></mark> cp/uL stock + 180 uL H2O --> 200 uL <mark><b>{conc_dilute_10x:.2e}</b></mark> cp/uL', unsafe_allow_html=True)
-        dilute_calc(conc_dilute_10x, wanted_conc) 
-    elif conc == float(final_conc) and volume < 50 and volume > 2:
-        volume = volume - 2
-        # st.write('2 uL ',conc,' + ',volume,'uL H2O --> ',volume+2,' E4 cp/uL')
-        st.markdown(f'2 uL <mark><b>{conc:.2e}</b></mark> cp/uL + <mark><b>{volume}</b></mark> uL H2O --> <mark><b>{volume+2}</b></mark> <mark><b>{wanted_conc:.2e}</b></mark> cp/uL',unsafe_allow_html=True)
-        st.markdown("Starting concentration might be too diluted. Consider changing the desired concentration or use a more concentrated stock.")
-    else:
-        st.markdown("Input an appropriate target concentration!")
-
+    if volume < 50 and count > 0:
+        conc = conc*10
+        dilute_calc(conc, wanted_conc)
+        return count
+        
 
 
 
@@ -92,4 +111,4 @@ st.write((final_conc))
 # Asking the final concentration wanted
 desired_conc = st.number_input("Final target concentration (cp/uL) ",value = 5.0, min_value=0.0, placeholder= "Input number here")
 
-dilute_calc(float(final_conc), desired_conc)
+dilute_calc(float(final_conc.iloc[0]), desired_conc)
